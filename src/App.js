c5ad8bb3c8a0c13e4ba1632753f895b712fa08a9
salwanurra <salwanurs09@gmail.com>
2022-09-13 22:26:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./App.css"
+
+const Home = () => {
+    const [data, setDatas] = useState(null);
+    const header = [
+        "Currency", "We Buy", "Exchange Rate", "We Sell"
+    ]
+
+    const getData = async () => {
+        return axios.get("https://api.currencyfreaks.com/latest?apikey=4c57df131ca04e3b913c78a1aa33ad0f&symbols=CAD,EUR,IDR,JPY,CHF,GBP");
+    };
+
+    useEffect(() => {
+        getData().then(res => setDatas(res.data.rates));
+    }, []);
+
+    return (
+      <div>
+        {
+          data ?
+            <div className="center">
+              <table>
+               <thead>
+                  <tr>
+                    { header.map((header, headerIdx) => {
+                      return <th key={headerIdx}>{header}</th>
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  { Object.keys(data).map((key, keyIdx) => (
+                      <tr key={keyIdx}>
+                        <td>{key}</td>
+                        <td>{(1.05 * data[key]).toFixed(4)}</td>
+                        <td>{(1.0 * data[key]).toFixed(4)}</td>
+                        <td>{(0.95 * data[key]).toFixed(4)}</td>
+                      </tr>
+                  ))}
+                </tbody>
+              </table>
+
+            </div>
+            :
+            <p>Loading</p>
+          }
+          <div style={{textAlign:"center"}}>
+            <p>Rates are based from 1 USD</p>
+            <p>This application uses API from https://currencyfreaks.com.</p>     
+          </div>
+      </div>
+    );
+}
+
+export default Home;
